fixed sign in cost, removed smoothing from mppi

src/controllers/mpc/mppi_jax.py
@@ -43,7 +43,7 @@ def rollout(
         u, v, bounded_noise = control
 
         new_x = x + dynamics(x, v) * step
-        new_S = S + cost(new_x, v, i) - gamma * jnp.einsum("n,nm,m->", u, inv_cv, bounded_noise)
+        new_S = S + cost(new_x, v, i) + gamma * jnp.einsum("n,nm,m->", u, inv_cv, bounded_noise)
         new_i = i + 1
 
         new_carry = new_x, new_S, new_i
@@ -206,11 +206,11 @@ class MPPI_Jax:
             self._forward_sim,
         )
 
-        u_padded = jnp.concatenate([self.u_history, u])
-        u_smoothed = jnp.array(savgol_filter(np.array(u_padded), 5, 3, axis=0)[-self.T :])
+        # u_padded = jnp.concatenate([self.u_history, u])
+        # u_smoothed = jnp.array(savgol_filter(np.array(u_padded), 5, 3, axis=0)[-self.T :])
 
-        self.u_history = jnp.roll(self.u_history, -1, axis=0)
-        self.u_history = self.u_history.at[-1].set(u[0])
+        # self.u_history = jnp.roll(self.u_history, -1, axis=0)
+        # self.u_history = self.u_history.at[-1].set(u[0])
         self.last_trajectory = u
 
-        return u_smoothed[0]
+        return u[0]

src/controllers/mpc/mppi_torch.py
@@ -93,7 +93,7 @@ class MPPI_Torch:
             # print(u.device, self.inv_cv.device, noise.device)
             S += (
                 self.cost(x, v[i, :], i)
-                - self.gamma
+                + self.gamma
                 * (u[i, :].unsqueeze(1) @ self.inv_cv @ noise[i, :].unsqueeze(2)).squeeze(-1).squeeze(-1)
             )
 
@@ -144,11 +144,11 @@ class MPPI_Torch:
         weighted_noise = torch.sum(weights.view(1, -1, 1) * noise, dim=1)
         u += weighted_noise
 
-        u_padded = torch.cat([self.u_history, u.cpu()])
-        u_smoothed = torch.from_numpy(savgol_filter(u_padded.cpu().numpy(), 5, 3, axis=0)[-self.T:])
-        # u = torch.from_numpy(u_smoothed[-self.T:])
-        self.u_history = torch.roll(self.u_history, -1, dims=0)
-        self.u_history[-1] = u_smoothed[0]
+        # u_padded = torch.cat([self.u_history, u.cpu()])
+        # u_smoothed = torch.from_numpy(savgol_filter(u_padded.cpu().numpy(), 5, 3, axis=0)[-self.T:])
+        # # u = torch.from_numpy(u_smoothed[-self.T:])
+        # self.u_history = torch.roll(self.u_history, -1, dims=0)
+        # self.u_history[-1] = u_smoothed[0]
         self.last_trajectory = u
 
-        return u_smoothed[0].numpy()
+        return u[0].numpy()

src/controllers/mpc/smppi_jax.py
@@ -44,7 +44,7 @@ def rollout(
         u, a, bounded_noise = control
 
         new_x = x + dynamics(x, a) * step
-        new_S = S + cost(new_x, a, i) - gamma * jnp.einsum("n,nm,m->", u, inv_cv, bounded_noise)
+        new_S = S + cost(new_x, a, i) + gamma * jnp.einsum("n,nm,m->", u, inv_cv, bounded_noise)
         new_i = i + 1
 
         new_carry = new_x, new_S, new_i