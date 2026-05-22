1cf46171e7a56ff8ff1573ca35408752a74db8a5
ds12a <dshen1248@gmail.com>
2026-05-21 21:32:13 -0400

    fwd/rev mppi works

--- experiments/exp_003_racecar_mppi/run_car_mppi.py
+++ experiments/exp_003_racecar_mppi/run_car_mppi.py
@@ -13,7 +13,7 @@ import jax.numpy as jnp
 
 
 def run_mpc():
-    scenario = "sample_oval.yaml" # sample_oval.yaml ks_barcelona_layout_gp_dallara_f317_rl_long
+    scenario = "ks_barcelona_layout_gp_dallara_f317_rl_long.yaml"  # sample_oval.yaml ks_barcelona_layout_gp_dallara_f317_rl_long
 
     env = RecordVideo(
         gym.make(
@@ -21,7 +21,7 @@ def run_mpc():
             scenario=f"package://scenarios/{scenario}",
             uncertainty=None,
             renderer="pybullet",
-            render_mode="rgb_array_follow",
+            render_mode="rgb_array_birds_eye",
             render_width=300,
             render_height=200,
         ),
@@ -35,7 +35,20 @@ def run_mpc():
     )
     dynamics, cost, bound = gen_util_funs(params[0])
 
-    mpc = MPPI_Jax(6, 3, dynamics, None, cost, bound, jnp.diag(jnp.array([0.25, 0.5, 0.5])), inverse_temp=0.5, K=1000, gamma=0.1, step = 0.05, T=30)
+    mpc = MPPI_Jax(
+        6,
+        2,
+        dynamics,
+        None,
+        cost,
+        bound,
+        jnp.diag(jnp.array([0.7, 0.5])),
+        inverse_temp=1,
+        K=500,
+        gamma=0.1,
+        step=0.05,
+        T=30,
+    )
 
     observation, reward, terminated, truncated, info = env.step(jnp.zeros(3))
 
@@ -49,13 +62,22 @@ def run_mpc():
             mpc_state = jnp.array([state.x, state.y, state.yaw, state.vx, state.vy, state.yaw_rate])
 
             u = mpc.run_mpc(mpc_state)
-            u.block_until_ready() 
+            u.block_until_ready()
 
-            print(i, time.perf_counter() - start, u, round(state.progress, 3), round(state.vx, 3), round(state.vy, 3))
+            print(
+                i,
+                time.perf_counter() - start,
+                u,
+                round(state.progress, 3),
+                round(state.vx, 3),
+                round(state.vy, 3),
+            )
             a = time.perf_counter()
-            observation, reward, terminated, truncated, info = env.step(u)
-            print(time.perf_counter() - a)
-            if terminated:
+
+            action = jnp.array([u[0], jnp.maximum(u[1], 0), -jnp.minimum(u[1], 0)])
+            observation, reward, terminated, truncated, info = env.step(action)
+
+            if terminated:  # or truncated:
                 break
 
             i += 1
